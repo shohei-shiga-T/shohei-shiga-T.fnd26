3e1a6343d441596b9f3fae7b167a23523626a9de
shohei-shiga-T <163956290+shohei-shiga-T@users.noreply.github.com>
2024-04-19 15:19:59 +0900

    Update weatherCheck.js
    時刻の情報取得を1時間ごとから10分ごとに変更。

--- weatherCheck.js
+++ weatherCheck.js
@@ -1,21 +1,21 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-// 時刻を取得
+// 時刻を取得する
 function addZeroToHead(num){
   if( num < 10){
     return "0" + ( num );
   }else {
-     return num
+     return num;
   };
 }
 
 function addZeroToHeadPlusOne(num){
   const month = num + 1;
   if( month < 10){
-    return "0" + month
+    return "0" + month;
   }else {
-     return month
+     return month;
   };
 }
 
@@ -23,9 +23,23 @@ const nowTime = new Date();
 const year = nowTime.getFullYear(); 
 const month = addZeroToHeadPlusOne(nowTime.getMonth());
 const date = addZeroToHead(nowTime.getDate());
-const hour = addZeroToHead(nowTime.getHours());
-const jmaTime = "" + year + month + date + hour + "0000";
-console.log(jmaTime)
+const hour = function() {
+  const hor = nowTime.getHours();
+  const min = nowTime.getMinutes();
+  if (min < 10){
+    return addZeroToHead(hor - 1);
+  } else{
+    return addZeroToHead(hor);
+  } 
+}
+const minutes = function() {
+  const minComv = [5,0,1,2,3,4];
+  const min = Math.floor(nowTime.getMinutes() / 10);
+  return minComv[min];
+} 
+
+const jmaTime = "" + year + month + date + hour() + minutes() + "000";
+console.log(jmaTime);
 
 // 地域コード
 const codeOfToyota = "51116";
@@ -41,7 +55,7 @@ async function getJmaStatus(){
   const response = await fetch(request);
   const jmaJSON = await response.json();
   
-  displayTemp(jmaJSON)
+  displayTemp(jmaJSON);
 }
 
 // jmaJSONから温度を読み取り数値と注意情報をwebページに表示
@@ -58,11 +72,11 @@ function displayTemp(object) {
 
   function alertCheck(temp){
     if (temp < 5) {
-      return "路面凍結注意"
+      return "路面凍結注意";
     } else if(temp > 28 ){
-      return "熱中症注意"
+      return "熱中症注意";
     } else {
-      return "注意情報はありません"
+      return "注意情報はありません";
     }
   }
  
